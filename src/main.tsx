c59a3eb5cfd29137eca760fed7e5c74f7e1e5640
Leonardo <leonardohibanhez@gmail.com>
2026-04-14 19:10:48 -0300

    ✨ New Feature: Criação da pagina da equipe

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Equipe from './pages/equipe/Equipe.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: 'equipe', element: <Equipe /> },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    safelist: [
+        "sm:w-[28rem]",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
