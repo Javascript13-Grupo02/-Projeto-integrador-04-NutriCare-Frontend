2884283e4998edc9eb33ef87e8f8b93e7a7e8b36
wallaceespy <wallace17.esp@gmail.com>
2026-04-14 17:53:01 -0300

    ✨ New Feature: Navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <div className='w-full flex justify-center py-4 bg-orange-800  text-white'>
+      <div className="container flex justify-between text-lg mx-8">
+
+        <Link to='/home' className="text-2xl font-bold">NutriCare</Link>
+
+        <div className='flex gap-4'>
+          <Link to='/sobre' className='hover:underline'>Sobre</Link>
+          <Link to='/equipe' className='hover:underline'>Equipe</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
