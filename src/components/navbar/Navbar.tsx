2942f74a06863fdfc760060d5d299b888cbfb6be
wallaceespy <wallace17.esp@gmail.com>
2026-04-15 09:24:39 -0300

    ✨ New Feature: hover:text-emerald-500

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
-    <div className='w-full flex justify-center py-4 bg-orange-800  text-white'>
+    <div className='w-full flex justify-center py-4 bg-orange-800 text-white'>
       <div className="container flex justify-between text-lg mx-8">
 
         <Link to='/home' className="text-2xl font-bold">NutriCare</Link>
 
         <div className='flex gap-4'>
-          <Link to='/sobre' className='hover:underline'>Sobre</Link>
-          <Link to='/equipe' className='hover:underline'>Equipe</Link>
+          <Link to='/sobre' className=' hover:text-emerald-500 transition-colors'>Sobre</Link>
+          <Link to='/equipe' className=' hover:text-emerald-500 transition-colors'>Equipe</Link>
         </div>
       </div>
     </div>
